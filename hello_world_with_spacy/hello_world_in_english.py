43784c483e6b330a1df6b11b687250686d2b44c7
Sukriti Sarkar <tosukriti5@gmail.com>
2019-10-23 19:53:59 +0530

    more example on tokenization

--- hello_world_with_spacy/hello_world_in_english.py
+++ hello_world_with_spacy/hello_world_in_english.py
@@ -15,3 +15,11 @@ first_token = doc[0]
 
 # Print the first token's text
 print(first_token.text)
+
+# A slice of the Doc for "Hello World"
+hello_world = doc[3:5]
+print(hello_world.text)
+
+# A slice of the Doc for "Hello World in English"
+hello_world_in_english = doc[3:7]
+print(hello_world_in_english.text)
